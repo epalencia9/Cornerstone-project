f0e5906b55f7254e59457b1f5baa503e6b260c1d
epalencia9 <120271561+epalencia9@users.noreply.github.com>
2023-03-14 14:16:46 -0500

    Update main.tf

--- Docker-terraform/main.tf
+++ Docker-terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
 resource "aws_instance" "oregon_server" {
   ami           = "ami-0df24e148fdb9f1d8"
   instance_type = "t2.micro"
@@ -7,7 +23,7 @@ resource "aws_instance" "oregon_server" {
   
 
   tags = {
-  Name = "Docker-terraform"
+  Name = "Docker-Terraform"
   }
   }
   
