a53885c3767538c0fa810c69e31c4452be02c1b0
epalencia9 <120271561+epalencia9@users.noreply.github.com>
2023-03-14 14:17:17 -0500

    Update docker.sh

--- Docker-terraform/docker.sh
+++ Docker-terraform/docker.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sudo yum install -y yum-utils
-sudo amazon-linux-extras install docker
+
+sudo yum update -y
+sudo amazon-linux-extras install docker -y
 sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo yum install -y git
-sudo curl -l https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl enable docker
+sudo usermod -a -G docker ec2
