04383444dc14bf00934282a2262ac64f6545a6e9
epalencia9 <120271561+epalencia9@users.noreply.github.com>
2023-03-14 14:18:21 -0500

    Update docker.sh

--- Docker-terraform/docker.sh
+++ Docker-terraform/docker.sh
@@ -3,4 +3,5 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
-sudo usermod -a -G docker ec2
+sudo usermod -a -G docker ec2-user
+sudo systemctl enable docker
