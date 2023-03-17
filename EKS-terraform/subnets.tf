fa091aa846c7c52880febce2076154e4a831ba6b
epalencia9 <120271561+epalencia9@users.noreply.github.com>
2023-03-16 20:40:31 -0500

    Update subnets.tf

--- EKS-terraform/subnets.tf
+++ EKS-terraform/subnets.tf
@@ -1,10 +1,10 @@
-resource "aws_subnet" "private-us-west-2b" {
+resource "aws_subnet" "private-us-west-2a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-west-2b"
+  availability_zone = "us-west-2a"
 
   tags = {
-    "Name"                            = "private-us-west-2b"
+    "Name"                            = "private-us-west-2a"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/demo"      = "owned"
   }
@@ -22,14 +22,14 @@ resource "aws_subnet" "private-us-west-2b" {
   }
 }
 
-resource "aws_subnet" "public-us-west-2b" {
+resource "aws_subnet" "public-us-west-2a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.64.0/19"
   availability_zone       = "us-west-2b"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                       = "public-us-west-2b"
+    "Name"                       = "public-us-west-2a"
     "kubernetes.io/role/elb"     = "1"
     "kubernetes.io/cluster/demo" = "owned"
   }
