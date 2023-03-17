7637a409be72de64ee0ab2a1e6f5d871167e9ab4
epalencia9 <120271561+epalencia9@users.noreply.github.com>
2023-03-16 20:41:22 -0500

    Update subnets.tf

--- EKS-terraform/subnets.tf
+++ EKS-terraform/subnets.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "private-us-west-2b" {
 resource "aws_subnet" "public-us-west-2a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.64.0/19"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
